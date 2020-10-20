9683ae34b9511fe900b895c5419f6d39ed8bb491
Reagan Brasch <reaganbrasch@gmail.com>
2020-10-20 22:51:18 +1100

    ex2-3, condensing the program

--- chapter2/dweight.c
+++ chapter2/dweight.c
@@ -2,18 +2,13 @@
 
 int main(void)
 {
-    int height, width, length, volume, weight;
-
-    height = 8;
-    length = 12;
-    width = 10;
+    int height = 8, length = 12, width = 10, volume; // (1)
 
     volume = height * length * width;
-    weight = (volume + 165) / 166;
 
     printf("Dimensions: %dx%dx%d\n", length, width, height);
     printf("Volume (cubic inches): %d\n", volume);
-    printf("Dimensional weight (pounds): %d\n", weight);
+    printf("Dimensional weight (pounds): %d\n", (volume + 165) / 166); // (2)
 
     return 0;
 }
