132682cd18d5cc70b17e76a6f505e88a5f0b70bc
Reagan Brasch <reaganbrasch@gmail.com>
2020-10-20 22:45:04 +1100

    exercise 2.2

---
+++ chapter1/ch1e2.c
@@ -0,0 +1,16 @@
+// a) directives and statements are noted
+// b) produces 3 lines of text:
+        // Parkinson's Law:
+        // Work expands so as to fill the time
+        // available for it's completion.
+
+#include <stdio.h> // directive
+
+int main(void)
+{
+    printf("Parkinson's Law:\nWork expands so as to "); // statement
+    printf("fill the time\n"); // statement
+    printf("available for it's completion.\n"); // statement
+
+    return 0; // statement
+}
