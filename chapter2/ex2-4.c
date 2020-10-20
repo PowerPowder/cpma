bdd8d068b0da2622cdcf3387a40d6238408fcf1b
Reagan Brasch <reaganbrasch@gmail.com>
2020-10-20 23:30:53 +1100

    ex2-4, int and floats without initalisers

---
+++ chapter2/ex2-4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b, c;
+    float d, e, f;
+
+    printf("ints: %d %d %d\n", a, b, c);
+    printf("floats: %f %f %f", d, e, f);
+    return 0;
+}
