b1bc5b88bfa89844f0fd6f66f2838b02c15c098f
reaganbrasch@gmail.com <reg@QUAD30-01.stedwards.local>
2020-11-02 10:12:01 +1100

    programming challenge 1

---
+++ chapter3/pp1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int d, m, y;
+
+    printf("Enter a date (mm/dd/yyyy): ");
+    scanf("%d/%d/%d", &m, &d, &y);
+    printf("You entered the date %d%02d%d", y, m, d);
+    return 0;
+}
