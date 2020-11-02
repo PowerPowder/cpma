9bbc0fccfb48c85f53c5b73acede14c7b6bd8876
reaganbrasch@gmail.com <reg@QUAD30-01.stedwards.local>
2020-11-02 11:09:50 +1100

    programming project 2 chapter 3

---
+++ chapter3/pp2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int item, d, m, y;
+    float price;
+
+    printf("Enter item number: ");
+    scanf("%d", &item);
+
+    printf("Enter unit price: ");
+    scanf("%f", &price);
+
+    printf("Enter purchase date (mm/dd/yyyy): ");
+    scanf("%d/%d/%d", &m, &d, &y);
+
+    printf("Item\t\tUnit\t\tPurchase\n\t\tPrice\t\tDate\n");
+    printf("%d\t\t$%7.2f\t%d%d%d\n", item, price, m, d, y);
+
+    return 0;
+}
