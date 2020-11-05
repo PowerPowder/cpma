472c24976c26f19273d2736c96ee834a7a41656e
PowerPowder <reg@QUAD30-01.stedwards.local>
2020-11-05 13:52:55 +1100

    programming project 2 chapter 4

--- chapter4/pp1.c
+++ chapter4/pp1.c
@@ -5,8 +5,8 @@ int main(void)
     int num;
 
     printf("Enter a two-digit number: ");
-    scanf("%2d", &num);
+    scanf("%3d", &num);
 
-    printf("The reversal is: %d%d", num % 10, num / 10);
+    printf("The reversal is: %d%d%d", num % 10, (num / 10) % 10, (num / 100) % 10);
     return 0;
 }
