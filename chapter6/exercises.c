929151016ed86920885676372dc1867279b02419
PowerPowder <reaganbrasch@gmail.com>
2020-11-23 11:43:55 +1100

    Exercises 6-9 chapter 6

--- chapter6/exercises.c
+++ chapter6/exercises.c
@@ -54,5 +54,27 @@ int main(void)
     // do {...} while (i < 10);
     // is not equivalent
 
+    // Exercise 6
+    // for (i = 1; i <= 128; i *= 2)
+        // printf("%d ", i);
+
+    // Exercise 7
+    // for (i = 9384; i > 0; i /= 10)
+        // printf("%d ", i);
+
+    // Exercise 8
+    // 10 5 3 2 1 1 1 ... (infinite loop)
+    // for (i = 10; i >= 1; i /= 2)
+        // printf("%d ", i++);
+
+    // Exercise 9
+    // i = 10;
+    // while (i >= 1)
+    // {
+        // printf("%d ", i++);
+        // i /= 2;
+    // }
+
+
     return 0;
 }
